8cab06725180f44d1fc01692e34b9e0ea5882d1e
k7111617-lang <k7111617@gmail.com>
2026-01-20 20:32:12 +0400

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a=5
+print("type of a:",type(a))
+b="chicken"
+print("type of b:",type(b))
+c=1.67
+print("type of c:",type(c))
+d=True
+print("type of d:",type(d))
---
+++ strings.py
@@ -0,0 +1,8 @@
+a="Jamaica"
+b="Trinidad"
+print(a+b)
+print(b*3)
+print(a[-1])
+print(b[0])
+print(a.upper())
+print(b.lower())
