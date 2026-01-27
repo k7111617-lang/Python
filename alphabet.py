68cf82a2d971379fa78024d15378362f4dd753ce
k7111617-lang <k7111617@gmail.com>
2026-01-27 20:34:50 +0400

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+character=input("Enter a character: ")
+if character>="A" and character<="Z":
+    print("It is an alphabet")
+elif character>="a" and character<="z":
+    print("It is an alphabet")
+else:
+    print("It isn't an alphabet")
