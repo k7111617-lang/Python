4c7a3126653174a874b87a752af1850f01969026
k7111617-lang <k7111617@gmail.com>
2026-05-10 11:53:04 +0400

    Add files via upload

---
+++ mathmodule.py
@@ -0,0 +1,8 @@
+import math 
+print("The floor of 67.42 is", math.floor(67.42))
+print("The ceil of 92.39 is", math.ceil(92.39))
+x=20
+y=-67
+print("The sign is copied to the first variable", math.copysign(x,y))
+print("The GCD of 84 and 20 are", math.gcd(20, 84))
+print("The FABS of -89 is", math.fabs(-89))
